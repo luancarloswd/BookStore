711a1059c0ea7f269e7a727314e67689b786adc5
luancarloswd <luancarloswd@gmail.com>
2015-07-10 21:01:43 -0300

    Initial Commit

---
+++ BookStore.Domain/Contracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.Contracts
+{
+    public interface IRepository<T> : IDisposable
+    {
+        List<T> Get(int skip = 0, int take = 25);
+        T Get(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
